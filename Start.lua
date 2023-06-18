de30638eb87b28c50db04f0d9998a2f5dfac1df9
flipx25 <136942908+TheFlip1@users.noreply.github.com>
2023-06-17 23:39:35 -0300

    Mudanças
    Alterei o Start, e mudei do tower of hell pra the mimic

--- Start.lua
+++ Start.lua
@@ -1,4 +1,10 @@
-local SimpleHacks = loadstring(game:HttpGet("https://raw.githubusercontent.com/Simple-Hacks/UI/main/Ui.lua?token=GHSAT0AAAAAAB5P4TK6ADJRKOSIL2FAYY3SY6EJN5A"))()
+local SimpleHacks = loadstring(game:HttpGet("https://raw.githubusercontent.com/Simple-Hacks/UI/main/Ui.lua?token=GHSAT0AAAAAACEBT6NUNPFGWCI3XPAEAHIUZEONXEQ"))()
+
+local Gets = {
+    ["SimpleHacks"] = loadstring(game:HttpGet("https://raw.githubusercontent.com/Simple-Hacks/UI/main/Ui.lua?token=GHSAT0AAAAAACEBT6NUNPFGWCI3XPAEAHIUZEONXEQ"))(),
+    ["The mimic"] = nil
+}
+
 local GUI = SimpleHacks:Create{
     Name = "Main",
     Size = UDim2.fromOffset(600, 400),
@@ -6,13 +12,13 @@ local GUI = SimpleHacks:Create{
     Link = "https://github.com/Simple-Hacks"
 }
 local Tab = GUI:Tab{
-	Name = "Tower of hell",
+	Name = "The Mimic",
 	Icon = "rbxassetid://12158562281"
 }
 Tab:Button{
-	Name = "Button",
+	Name = "The mimic Script",
 	Description = nil,
 	Callback = function()
-        warn("test")
+
     end
 }
