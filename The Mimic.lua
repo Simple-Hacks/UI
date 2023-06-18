6e14e7fc13bc453a69b9ac5c90f460d8bbb5faba
flipx25 <136942908+TheFlip1@users.noreply.github.com>
2023-06-18 00:05:38 -0300

    Update The Mimic.lua

--- The Mimic.lua
+++ The Mimic.lua
@@ -1,3 +1,13 @@
-return function ()
-    warn("Penis")
+local TheMimicScript = {}
+TheMimicScript.__index = TheMimicScript
+
+function TheMimicScript.New (TestValue)
+    local self = setmetatable({},TheMimicScript)
+
+    self.TestValue = TestValue
+    warn(self.TestValue)
+
+    return self
 end
+
+return TheMimicScript
